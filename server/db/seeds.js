35730f481db429d9db905a84ac53ccb8318b30ea
Allan Milne <amilne.wd@gmail.com>
2019-09-18 11:01:48 +0100

    adds seed db file

---
+++ server/db/seeds.js
@@ -0,0 +1,20 @@
+use bookings;
+db.dropDatabase();
+
+db.bookings.insertMany([
+  {
+    name: "Davey Attenborough",
+    email: "davey@animalwatching.com",
+    checkedIn: true
+  },
+  {
+    name: "Amanda Williamson",
+    email: "amanda@gmail.com",
+    checkedIn: false
+  },
+    {
+    name: "Cameron Pellett",
+    email: "cammy@gmail.com",
+    checkedIn: false
+  }
+]);

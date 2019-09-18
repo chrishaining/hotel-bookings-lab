b4705ff005a8ff010e7c43352b74f3d56f7f278c
Allan Milne <amilne.wd@gmail.com>
2019-09-18 13:43:40 +0100

    break time

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello world</h1>
+    <bookings-grid :bookings="bookings" /> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import BookingService from './services/BookingService'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+    bookings: []
+    }
+  },
+methods: {
+  fetchData() {
+    BookingService.getBookings()
+    .then(bookings => this.bookings = bookings);
   }
+}
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
